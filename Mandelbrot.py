ce6e9bf9b03328dbf1c1d3bb07b3afbb0c3772c1
Albert Fajula <fajulanet@gmail.com>
2015-11-19 11:20:05 +0100

    Finish gif conversion

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -6,8 +6,7 @@ Created on Mon Nov 09 10:48:45 2015
 """
 
 from __future__ import division
-from pylab import*
-import matplotlib.animation as animation
+from pylab import *
 import time
 import os
 
@@ -42,6 +41,7 @@ for i in range(0,90,10):
     axis('off')
     imshow(cm, cmap='gray')
 
+
 zs=zeros([M,M])
 ims=[]
 for i in range(0,90):
@@ -51,21 +51,21 @@ for i in range(0,90):
     k=(abs(zs)<top)
     cmsuma=cmsuma+k
     ims.append(cmsuma)
-    imsave("cmsuma"+str(i)+".png", cmsuma, cmap="prism")
+    if len(str(i))==1:
+        h='0'+str(i)
+    else:
+        h=str(i)
+    imsave('cmsuma'+h+'.png', cmsuma, cmap='prism')
 # follow:http://docs.wand-py.org/en/0.4.1/guide/install.html#install-imagemagick-on-windows
 #os.system("IMconvert -delay 20 -loop 0 *png mandelbrot.gif")
 
-# Set up formatting for the movie files
-Writer = animation.MovieWriterRegistry()
-writer = animation.MovieWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-
-fig=figure()
-animation.ArtistAnimation(fig, ims, interval=50, repeat_delay=3000,blit=True)
-im_ani.save('im.mp4', writer=writer)
+os.system('convert.im6 -delay 50 -loop 0 *.png mandelbrot.gif')
+os.system('rm *.png')
 
 figure(1)
 axis('off')
 imshow(cmsuma, cmap='prism')
+imsave('mandelbrot90.png', cmsuma, cmap='prism')
 '''
 figure()
 imshow(abs(c)+angle(c))
--- diferencials.py
+++ diferencials.py
@@ -7,3 +7,5 @@ Created on Thu Nov 19 09:43:19 2015
 
 from __future__ import division
 import pylab*
+from scipy.integration import odeint
+
